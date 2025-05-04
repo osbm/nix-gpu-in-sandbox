9bc00b020581ca27d682538fa8eee4c50010dee5
osbm <osmanfbayram@gmail.com>
2025-05-04 18:25:02 +0300

    add initial working example

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A flake that showcases how to use nvidia GPUs inside nix sandbox";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    };
+  in
+  {
+    packages.x86_64-linux.nvidia-smi = pkgs.stdenv.mkDerivation {
+      name = "nvidia-smi-runner";
+      version = "0.0.0";
+
+      src = null;
+      unpackPhase = "true";
+
+      cudaSupport = true;
+      requiredSystemFeatures = [ "cuda" ];
+      env = let
+        libs = [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.xorg.libX11
+          pkgs.ncurses5
+          pkgs.linuxPackages.nvidia_x11_beta
+          pkgs.libGL
+          pkgs.libzip
+          pkgs.glib
+        ];
+      in
+      {
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libs}";
+        CUDA_PATH = "${ pkgs.cudatoolkit}";
+      };
+      nativeBuildInputs = [
+        (pkgs.python312.withPackages (
+          ppkgs: [ pkgs.python312Packages.torchWithCuda ]
+        ))
+      ];
+
+      buildPhase = ''
+        mkdir -p $out
+        python -c "import torch; print(f'{torch.cuda.is_available()=}')"
+        ${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi > $out/output.log
+      '';
+    };
+  };
+}
