31d59870a64bbeed2886e23625b9e82669f44e99
osbm <osmanfbayram@gmail.com>
2025-05-04 20:06:14 +0300

    formatting

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A flake that showcases how to use nvidia GPUs inside nix sandbox";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { nixpkgs, ... }: let
+  outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
@@ -10,8 +10,7 @@
         cudaSupport = true;
       };
     };
-  in
-  {
+  in {
     packages.x86_64-linux.nvidia-smi = pkgs.stdenvNoCC.mkDerivation {
       name = "nvidia-smi-runner";
       version = "0.0.0";
@@ -20,13 +19,13 @@
       unpackPhase = "true";
 
       cudaSupport = true;
-      requiredSystemFeatures = [ "cuda" ];
+      requiredSystemFeatures = ["cuda"];
       env = {
-        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.linuxPackages.nvidia_x11_beta ]}";
+        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.linuxPackages.nvidia_x11_beta]}";
       };
       nativeBuildInputs = [
         (pkgs.python312.withPackages (
-          ppkgs: [ pkgs.python312Packages.torchWithCuda ]
+          ppkgs: [pkgs.python312Packages.torchWithCuda]
         ))
       ];
 
